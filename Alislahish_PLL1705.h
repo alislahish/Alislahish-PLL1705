56279e6ebba7d6021647eb658b88fe5d7f3e6fe2
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2015-07-26 01:15:17 -0400

    header file changes
    update comments for reference purposes

--- Alislahish_PLL1705.h
+++ Alislahish_PLL1705.h
@@ -1,17 +1,17 @@
-/*
-
- Alislahish_PLL1705.h
- by anwar hahj jefferson-george
-
- This file can be used to represent the functions and settings of the
-
-  PLL1705 3.3V Dual PLL Multiclock Generator by Burr Brown Products from Texas Instruments.
-
-  Product page:
-  http://www.ti.com/product/PLL1705?keyMatch=pll1705&tisearch=Search-EN-Everything
-  Datasheet (Rev. A):
-  http://www.ti.com/lit/gpn/pll1705
-
+/**
+*
+* Alislahish_PLL1705.h
+* by anwar hahj jefferson-george
+*
+* This file can be used to represent the functions and settings of the
+*
+*  PLL1705 3.3V Dual PLL Multiclock Generator by Burr Brown Products from Texas Instruments.
+*
+*  Product page:
+*  http://www.ti.com/product/pll1705
+*  Datasheet (Rev. A):
+*  http://www.ti.com/lit/gpn/pll1705
+*
 */
 #include "Arduino.h"
 #include "ICUsingMCP23017.h"
@@ -20,6 +20,7 @@
 #ifndef ALISLAHISH_PLL1705_H
 #define ALISLAHISH_PLL1705_H
 
+//valid sampling frequencies this generator can be used with
 enum class PLLSamplingFrequencies{
 	KHZ_32000 = 0x00,
 	KHZ_44100 = 0x01,
@@ -29,11 +30,13 @@ enum class PLLSamplingFrequencies{
 	KHZ_96000 = 0x05
 };
 
+//generator sampling rates
 enum class PLLSamplingRates{
 	SINGLE = 0x01,
 	DOUBLE = 0x02
 };
 
+//sampling rates for the SCKO1 output 
 enum class SCKO1SamplingRates{
 	MHZ_33 = 0x00, //Low 33.8688 MHz
 	MHZ_16 = 0x01 //High 16.9344 MHz
@@ -54,10 +57,10 @@ class Alislahish_PLL1705 : public ICUsingMCP23017
 		PLLSamplingRates _sr;
 		SCKO1SamplingRates _scko1;
 		//hardware connects
-		uint8_t _CSELpin;
-		uint8_t _FS1pin;
-		uint8_t _FS2pin;
-		uint8_t _SRpin;
+		uint8_t _CSELpin;	//pin 12
+		uint8_t _FS1pin; 	//pin 5
+		uint8_t _FS2pin; 	//pin 6
+		uint8_t _SRpin;  	//pin 7
 
 		void setPinModes();
 		void setPins();
